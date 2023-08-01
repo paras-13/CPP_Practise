75b91a41c38f4548c4ac018c1748681f1cd48e61
Paras Upadhyay <paras13upadhyay@gmail.com>
2023-08-01 22:46:04 +0530

    CPP Problem1

---
+++ problem1.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string name = "Paras Upadhyay";
+    int age = 18;
+    int roll = 15;
+    cout << "Name :-  " << name << endl << "Age :- " << age << endl << "Roll_No :- " << roll;
+}
